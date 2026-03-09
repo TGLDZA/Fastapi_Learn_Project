5a2443ebd2c6a1c232d8207a4f60a3410b51a037
TGLDZA <2012279569@qq.com>
2026-03-09 21:28:27 +0800

    整合根据token查询用户方法进工具软件包

--- utils/auth.py
+++ utils/auth.py
@@ -1,2 +1,21 @@
+from fastapi import Header, Depends, HTTPException
+from sqlalchemy.ext.asyncio import AsyncSession
+from starlette import status
+
+from config.db_conf import get_db
+from crud.users import get_user_by_token
+
 
 # 整合 根据 Token 查询用户 返回用户
+async def get_current_user(
+    authorization: str = Header(..., alias="Authorization"),
+    db: AsyncSession = Depends(get_db)
+):
+    # 由于前端传的请求头字段中 token 格式为 Bearer <token>，所以需要去掉前面的"Bearer "字段才能获得token
+    token = authorization.replace("Bearer ", "")
+    user = await get_user_by_token(db, token)
+
+    if not user:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="无效的令牌或者令牌已过期")
+
+    return user
