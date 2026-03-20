6454f87e14e5536f7844b821b4622e26fb36dfa5
TGLDZA <2012279569@qq.com>
2026-03-20 14:52:31 +0800

    增加检查收藏状态路由函数，并挂载路由

--- routers/favorite.py
+++ routers/favorite.py
@@ -1,3 +1,17 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Query, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+from config.db_conf import get_db
+from models.users import User
+from utils.auth import get_current_user
+from utils.response import success_response
 
 router = APIRouter(prefix="/api/favorite", tags=["favorite"])
+
+@router.get("/check")
+async def check_favorite(
+        news_id: int = Query(..., alias="newsId"),
+        user: User = Depends(get_current_user),
+        db: AsyncSession = Depends(get_db)
+):
+
+    return success_response(message="检查收藏状态成功")
