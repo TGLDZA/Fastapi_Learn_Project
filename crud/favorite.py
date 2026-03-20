6f3d4b6f324799126a12402051dfe325593886fa
TGLDZA <2012279569@qq.com>
2026-03-20 16:12:39 +0800

    新增取消收藏crud函数

--- crud/favorite.py
+++ crud/favorite.py
@@ -25,3 +25,13 @@ async def add_news_favorite(
     await db.commit()
     await db.refresh(favorite)
     return favorite
+
+async def remove_news_favorite(
+        db: AsyncSession,
+        user_id: int,
+        news_id: int
+):
+    stmt = select(Favorite).where(Favorite.user_id == user_id, Favorite.news_id == news_id)
+    result = await db.execute(stmt)
+    await db.commit()
+    return result.rowcount > 0
