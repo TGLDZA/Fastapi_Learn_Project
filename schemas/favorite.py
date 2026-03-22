c309d8565f6d2ee5811cb9ec2beb069a9dabea69
TGLDZA <2012279569@qq.com>
2026-03-22 21:26:36 +0800

    新增获取收藏列表响应模型类

--- schemas/favorite.py
+++ schemas/favorite.py
@@ -1,4 +1,5 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, ConfigDict
+
 
 # 构造检查收藏状态的pydantic对象
 class FavoriteCheckResponse(BaseModel):
@@ -6,3 +7,14 @@ class FavoriteCheckResponse(BaseModel):
 
 class FavoriteAddRequest(BaseModel):
     news_id: int = Field(..., alias="newsId")
+
+# 收藏列表接口的响应模型类
+class FavoriteListResponse(BaseModel):
+    list: list[xx]
+    total: int
+    hasmore: bool = Field(alias="hasMore")
+
+    model_config = ConfigDict(
+        populate_by_name=True,
+        from_attributes=True
+    )
