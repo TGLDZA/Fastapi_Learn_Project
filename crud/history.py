b2ebb38001e66efd0bf02ebfacd890f008883d13
TGLDZA <2012279569@qq.com>
2026-03-23 12:40:12 +0800

    fix

--- crud/history.py
+++ crud/history.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+
+from sqlalchemy import select, update
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from models.history import History
@@ -8,6 +11,22 @@ async def add_news_history(
         user_id: int,
         news_id: int
 ):
+    stmt = select(History).where(History.user_id == user_id, History.news_id == news_id)
+    result = await db.execute(stmt)
+    existing_history = result.scalar_one_or_none()
+    # if existing_history:
+    #     stmt = (update(History)
+    #             .where(History.user_id == user_id, History.news_id == news_id)
+    #             .values(view_time=datetime.now()))
+    #     new_history = await db.execute(stmt)
+    #     await db.commit()
+    #     return new_history
+    if existing_history:
+        existing_history.view_time = datetime.now()
+        await db.commit()
+        await db.refresh(existing_history)
+        return existing_history
+
     history = History(user_id=user_id, news_id=news_id)
     db.add(history)
     await db.commit()
