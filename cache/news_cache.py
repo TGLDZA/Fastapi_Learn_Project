255672f112e85653af9d5f0fc0de8f2c635cbd7b
TGLDZA <2012279569@qq.com>
2026-03-23 17:13:29 +0800

    新增缓存中的新闻列表读取和写入方法

--- cache/news_cache.py
+++ cache/news_cache.py
@@ -1,12 +1,13 @@
 # 新闻相关的缓存方法
-from typing import Any, List, Dict
+from typing import Any, List, Dict, Optional
 
 from config.cache_conf import get_json_cache, set_cache
 
-# 新闻分类的读取和写入
-
+# key要保证唯一性
 CATEGORIES_KEY = "news:categories"
+NEWS_LIST_KEY_PREFIX = "news_list:"
 
+# 新闻分类的读取和写入
 
 # 获取新闻分类缓存
 async def get_cached_categories():
@@ -18,3 +19,18 @@ async def get_cached_categories():
 # 避免key同时过期，引起缓存雪崩
 async def set_cached_categories(data: List[Dict[str, Any]], expire: int = 7200):
     return await set_cache(CATEGORIES_KEY, data, expire)
+
+
+# 新闻列表的读取和写入
+# 写入缓存：新闻列表key：news_list:分类id:页码:该页大小  value: 新闻列表  expire:1800
+async def set_cached_news_list(category_id: Optional[int], page:int, page_size: int, news_list: List[Dict[str, Any]], expire: int = 1800):
+    # 调用封装的 Redis 设置和读取方法，存新闻列表到缓存
+    category_part = category_id if category_id is not None else "all"
+    key = f"{NEWS_LIST_KEY_PREFIX}{category_part}:{page}:{page_size}"
+    return await set_cache(key, news_list, expire)
+
+# 读取缓存--新闻列表
+async def get_cached_news_list(category_id: Optional[int], page:int, page_size: int):
+    category_part = category_id if category_id is not None else "all"
+    key = f"{NEWS_LIST_KEY_PREFIX}{category_part}:{page}:{page_size}"
+    return await get_json_cache(key)
