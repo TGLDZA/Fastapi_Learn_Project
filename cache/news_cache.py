046cdffec2eefcee91f7ef069eb397143b2c1c32
TGLDZA <2012279569@qq.com>
2026-03-23 16:36:21 +0800

    fix

--- cache/news_cache.py
+++ cache/news_cache.py
@@ -15,5 +15,6 @@ async def get_cached_categories():
 # 写入新闻分类缓存：缓存的数据，过期时间
 # 分类、配置：7200； 列表：600；详情：1800；验证码：120
 # 一般数据越稳定，缓存越持久
+# 避免key同时过期，引起缓存雪崩
 async def set_cached_categories(data: List[Dict[str, Any]], expire: int = 7200):
     return await set_cache(CATEGORIES_KEY, data, expire)
