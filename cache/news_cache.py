1b6fce3c564ec5c13836a1038e55f5f068ea2cb3
TGLDZA <2012279569@qq.com>
2026-03-23 16:16:25 +0800

    封装新闻分类缓存读取和写入方法

---
+++ cache/news_cache.py
@@ -0,0 +1,19 @@
+# 新闻相关的缓存方法
+from typing import Any, List, Dict
+
+from config.cache_conf import get_json_cache, set_cache
+
+# 新闻分类的读取和写入
+
+CATEGORIES_KEY = "news:categories"
+
+
+# 获取新闻分类缓存
+async def get_cached_categories():
+    await get_json_cache(CATEGORIES_KEY)
+
+# 写入新闻分类缓存：缓存的数据，过期时间
+# 分类、配置：7200； 列表：600；详情：1800；验证码：120
+# 一般数据越稳定，缓存越持久
+async def set_cached_categories(data: List[Dict[str, Any]], expire: int = 7200):
+    return await set_cache(CATEGORIES_KEY, data, expire)
